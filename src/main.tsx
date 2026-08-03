a1b57be0cf180e9c9191d12d4a9f77a4263eba50
Marcelo Rodrigo <rodrigomarcelo643@gmail.com>
2026-08-03 15:48:54 +0800

    chore: update main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { APIProvider } from '@vis.gl/react-google-maps'
+import { AuthProvider } from './context/AuthContext'
+import { ModalProvider } from './context/ModalContext'
+import { store } from './redux/store'
+import AppRouter from './routes'
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+        <AuthProvider>
+          <ModalProvider>
+            <AppRouter />
+          </ModalProvider>
+        </AuthProvider>
+      </APIProvider>
+    </Provider>
   </StrictMode>,
 )
